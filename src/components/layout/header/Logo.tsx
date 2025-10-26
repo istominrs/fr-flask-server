d507e292e82ab39c838b492ca695231ea37ff347
Roman Istomin <9872105@mail.ru>
2025-10-26 19:32:09 +0300

    add bull logo

--- src/components/layout/header/Logo.tsx
+++ src/components/layout/header/Logo.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Image from 'next/image'
 import Link from 'next/link'
 
 export function Logo() {
@@ -8,6 +9,7 @@ export function Logo() {
 			href='/'
 			className='flex items-center gap-x-4 transition-opacity hover:opacity-75'
 		>
+			<Image src='/images/logo.svg' alt='TeaTrade' width={40} height={40} />
 			<div className='hidden leading-tight lg:block'>
 				<h2 className='text-accent-foreground text-lg font-semibold tracking-wider'>
 					TeaTrade
