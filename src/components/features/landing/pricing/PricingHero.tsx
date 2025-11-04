3480948cca6f713e236fa4bfc09ca86c02b587a3
Roman Istomin <9872105@mail.ru>
2025-11-04 13:32:01 +0300

    remove useless div

--- src/components/features/landing/pricing/PricingHero.tsx
+++ src/components/features/landing/pricing/PricingHero.tsx
@@ -3,11 +3,6 @@
 export function PricingHero() {
 	return (
 		<section className='mt-28 mb-16 flex flex-col items-center justify-center text-center sm:mt-40'>
-			<div className='mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50'>
-				<p className='text-sm font-semibold text-gray-700'>
-					7-day free trial on all plans
-				</p>
-			</div>
 			<h1 className='max-w-4xl text-4xl font-bold text-gray-900 sm:text-5xl lg:text-6xl'>
 				Simple, transparent <span className='text-blue-600'>pricing</span>
 			</h1>
